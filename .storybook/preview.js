c1b1da8e2cf94ba41daecb1b4f10deebd7b13ac5
Pedro Azevedo <theazevedo@outlook.com>
2021-04-28 15:15:05 -0300

    adjust preview from storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,7 @@
 import { addDecorator } from '@storybook/react'
 import { withNextRouter } from 'storybook-addon-next-router'
 import { ThemeProvider } from 'styled-components'
-import GlobalStyles from '../src/styles/global'
+import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 
 addDecorator(withNextRouter())
