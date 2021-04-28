2e6dca1c30f056d2ee2e16f28fe3482a659cfef4
Pedro Azevedo <theazevedo@outlook.com>
2021-04-28 15:06:07 -0300

    creating theme.ts and use theme provider

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,23 +1,48 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+    @font-face {
+      font-family: 'Montserrat';
+      font-style: normal;
+      font-weight: 400;
+      font-display: swap;
+      src: local(''),
+          url('/fonts/montserrat-v15-latin-regular.woff2') format('woff2'),
+    }
+    @font-face {
+      font-family: 'Montserrat';
+      font-style: normal;
+      font-weight: 500;
+      font-display: swap;
+      src: local(''),
+          url('/fonts/montserrat-v15-latin-500.woff2') format('woff2'),
+    }
+    @font-face {
+      font-family: 'Montserrat';
+      font-style: normal;
+      font-weight: 700;
+      font-display: swap;
+      src: local(''),
+          url('/fonts/montserrat-v15-latin-700.woff2') format('woff2'),
+    }
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
+  ${({ theme }) => css`
+    html {
+      font-size: 62.5%;
+    }
 
-  html {
-    font-size: 62.5%;
-  }
-
-  html, body, #__next {
-    height: 100%;
-  }
-
-  body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
-  }
+    body {
+      font-family: ${theme.font.family};
+      font-size: ${theme.font.sizes.medium};
+    }
+  `}
 `
 
 export default GlobalStyles
---
+++ src/styles/theme.ts
@@ -0,0 +1,51 @@
+export default {
+  grid: {
+    container: '130rem',
+    gutter: '3.2rem'
+  },
+  border: {
+    radius: '0.4rem'
+  },
+  font: {
+    family:
+      "Montserrat, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    regular: 'regular',
+    normal: 500,
+    bold: 700,
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '1.8rem',
+      xlarge: '2.0rem',
+      xxlarge: '2.8rem'
+    }
+  },
+  colors: {
+    mainBg: '#454545',
+    white: '#FFFFFF',
+    black: '#333333',
+    mega: '#6BEFA3',
+    quina: '#8666EF',
+    lotoFacil: '#DD7AC6',
+    lotoMania: '#FFAB64',
+    timeMania: '#5AAD7D',
+    diaSorte: '#BFAF83'
+  },
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem'
+  },
+  layers: {
+    base: 10,
+    menu: 20,
+    overlay: 30,
+    modal: 40,
+    alwaysOnTop: 50
+  }
+}
